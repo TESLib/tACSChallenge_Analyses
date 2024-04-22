5caa53047c6f799f15b0ca3de5366cc9a2f8c489
BZoefel <103512548+BZoefel@users.noreply.github.com>
2024-04-22 13:25:56 +0200

    Add files via upload
    added permutations and preferred phase analysis

--- tACSChallenge_EvalData.m
+++ tACSChallenge_EvalData.m
@@ -1,12 +1,17 @@
-function [ps,bs,hit_probs] = tACSChallenge_EvalData(sorted_data)
+function [ps,bs,hit_probs,bs_perm,pf] = tACSChallenge_EvalData(sorted_data,perm,subjno)
 %% script originally written by Benedikt Zoefel, CNRS Toulouse, in October 2021
+%% April 24: added permutations and preferred phase
 
 %% sorted_data is the output from tACSChallenge_SortData
 
 n_cond = length(sorted_data);% number of conditions to be analysed
 ps = zeros(n_cond,1);
 bs = zeros(n_cond,1);
+bs_perm = zeros(n_cond,perm);
 hit_probs = zeros(8,n_cond);
+pf = zeros(n_cond,1); % preferred phase
+
+rng(subjno);
 
 for c = 1:n_cond
     
@@ -23,12 +28,29 @@ for c = 1:n_cond
     %p-value for phasic modulation of perception for single participant
     ps(c) = coefTest(g1);
     
+    % extract the preferred phase fom the fit
+    pf(c) = -1.*angle(g1.Coefficients.Estimate(3)+g1.Coefficients.Estimate(2)*1i);
+    
+    % regression coefficient
     curr_b = mnrfit(glm_input(:,1:2),glm_input(:,3)+1);
     % the higher the regression coefficient, the stronger the phasic
     % modulation of perception
     bs(c) = sqrt(curr_b(2)^2 + curr_b(3)^2);
-
-    %% divide detection probability into phase bins (mostly for visualisation)
+    
+    %% permutation procedure - shuffle phases
+    for shuff = 1:perm
+        shuffled_phases = curr_data(randperm(size(curr_data,1)),4);
+        glm_input(:,1) = sin(shuffled_phases);
+        glm_input(:,2) = cos(shuffled_phases);
+        glm_input(:,3) = curr_data(:,2);
+                  
+        curr_b = mnrfit(glm_input(:,1:2),glm_input(:,3)+1);
+        % the higher the regression coefficient, the stronger the phasic
+        % modulation of perception
+        bs_perm(c,shuff) = sqrt(curr_b(2)^2 + curr_b(3)^2);
+    end
+        
+    %% divide detection probability into phase bins (mostly for visualisation) - number of phases fixed to 8 here
     for p = 2:8
         hit_probs(p,c) = sum(curr_data(curr_data(:,5)==p,2))/sum(curr_data(:,5)==p);
     end
